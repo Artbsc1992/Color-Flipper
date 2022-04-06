9709f43293402142ff7200df444ab13bd333617a
Arturo <ahpareja@gmail.com>
2022-04-05 22:13:10 -0500

    Finish project

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const color = document.querySelector(".color");
 
 btn.addEventListener('click', function (){
     //get random number between 0 - 3 [1]
-    const randomNumer = 2;
-    document.body.style.backgroundColor = color [randomNumer];
-    console.log(color.textContent)
-})
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors [randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
